2cde6020b4a1c498e103a9d39dea94a019abfd18
prjulia <37224090+prjulia@users.noreply.github.com>
2018-03-10 18:05:39 +0300

    файлы для сайта

---
+++ jQuery/jquery1.js
@@ -0,0 +1,29 @@
+ $(document).ready(function(){
+        $("#choice").mouseover(function(){$("#choice").css("border","2px solid #ee6e42");$("#choice").css("border-radius","5px");});
+		$("#choice").mouseout(function(){$("#choice").css("border","none")});
+		$("#choice").click(function(){$("#example").attr("src","png/example_1.png");$("#example_name").html("Ирландский дуб");});
+		
+		$("#choice1").mouseover(function(){$("#choice1").css("border","2px solid #ee6e42");$("#choice1").css("border-radius","5px");});
+		$("#choice1").mouseout(function(){$("#choice1").css("border","none")});
+		$("#choice1").click(function(){$("#example").attr("src","png/example_2.png");$("#example_name").html("Вишня");});
+		
+		$("#choice2").mouseover(function(){$("#choice2").css("border","2px solid #ee6e42");$("#choice2").css("border-radius","5px");});
+		$("#choice2").mouseout(function(){$("#choice2").css("border","none")});
+		$("#choice2").click(function(){$("#example").attr("src","png/example_3.png");$("#example_name").html("Макоре");});
+		
+		$("#choice3").mouseover(function(){$("#choice3").css("border","2px solid #ee6e42");$("#choice3").css("border-radius","5px");});
+		$("#choice3").mouseout(function(){$("#choice3").css("border","none")});
+		$("#choice3").click(function(){$("#example").attr("src","png/example_4.png");$("#example_name").html("Липа");});
+		
+		$("#choice4").mouseover(function(){$("#choice4").css("border","2px solid #ee6e42");$("#choice4").css("border-radius","5px");});
+		$("#choice4").mouseout(function(){$("#choice4").css("border","none")});
+		$("#choice4").click(function(){$("#example").attr("src","png/example_5.png");$("#example_name").html("Амарант");});
+		
+		$("#choice5").mouseover(function(){$("#choice5").css("border","2px solid #ee6e42");$("#choice5").css("border-radius","5px");});
+		$("#choice5").mouseout(function(){$("#choice5").css("border","none")});
+		$("#choice5").click(function(){$("#example").attr("src","png/example_6.png");$("#example_name").html("Берлиния");});
+		
+		$("#choice6").mouseover(function(){$("#choice6").css("border","2px solid #ee6e42");$("#choice6").css("border-radius","5px");});
+		$("#choice6").mouseout(function(){$("#choice6").css("border","none")});
+		$("#choice6").click(function(){$("#example").attr("src","png/example_7.png");$("#example_name").html("Габон");});
+});
